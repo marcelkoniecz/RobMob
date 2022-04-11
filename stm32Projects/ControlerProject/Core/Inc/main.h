2c8928ca382ad2e97feb531ca32dd49b4351388c
Kuchnast <62248017+kuchnast@users.noreply.github.com>
2022-04-12 00:36:30 +0200

    Add documentation of controler

--- stm32Projects/ControlerProject/Core/Inc/main.h
+++ stm32Projects/ControlerProject/Core/Inc/main.h
@@ -58,10 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define BLTTH_TX_Pin GPIO_PIN_6
-#define BLTTH_TX_GPIO_Port GPIOB
-#define BLTTH_RX_Pin GPIO_PIN_7
+#define BLTTH_RX_Pin GPIO_PIN_6
 #define BLTTH_RX_GPIO_Port GPIOB
+#define BLTTH_TX_Pin GPIO_PIN_7
+#define BLTTH_TX_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- stm32Projects/ControlerProject/Core/Src/usart.c
+++ stm32Projects/ControlerProject/Core/Src/usart.c
@@ -65,7 +65,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     PB6     ------> USART1_TX
     PB7     ------> USART1_RX
     */
-    GPIO_InitStruct.Pin = BLTTH_TX_Pin|BLTTH_RX_Pin;
+    GPIO_InitStruct.Pin = BLTTH_RX_Pin|BLTTH_TX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -93,7 +93,7 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
     PB6     ------> USART1_TX
     PB7     ------> USART1_RX
     */
-    HAL_GPIO_DeInit(GPIOB, BLTTH_TX_Pin|BLTTH_RX_Pin);
+    HAL_GPIO_DeInit(GPIOB, BLTTH_RX_Pin|BLTTH_TX_Pin);
 
   /* USER CODE BEGIN USART1_MspDeInit 1 */
 
